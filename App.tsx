5bf9289aabdc2557c2c31bfcb8783bb26cccdf85
amanmehra262001 <amanmehra262001@gmail.com>
2024-10-19 17:34:58 +0530

    update: setup for comet chat

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -15,6 +15,9 @@ import {
   Text,
   useColorScheme,
   View,
+  Alert,
+  TextInput,
+  Button,
 } from 'react-native';
 
 import {
@@ -25,12 +28,24 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import {Platform, PermissionsAndroid} from 'react-native';
+import {
+  CometChatIncomingCall,
+  CometChatOngoingCall,
+  CometChatOutgoingCall,
+  CometChatUIKit,
+  UIKitSettings,
+} from '@cometchat/chat-uikit-react-native';
+import {CometChat} from '@cometchat/chat-sdk-react-native';
+import {CometChatCalls} from '@cometchat/calls-sdk-react-native';
+
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
 function Section({children, title}: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+
   return (
     <View style={styles.sectionContainer}>
       <Text
@@ -58,6 +73,284 @@ function Section({children, title}: SectionProps): React.JSX.Element {
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
+  const [callReceived, setCallReceived] = useState(false);
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [callRejected, setCallRejected] = useState(false);
+  const [activeCall, setActiveCall] = useState(null);
+  const [cometchatLoginUID, setCometchatLoginUID] = useState('');
+  const [cometchatUser, setCometchatUser] = useState(null);
+  const [sessionId, setSessionId] = useState('');
+
+  const incomingCall = useRef(null);
+
+  // add below code in App.js or App.tsx
+  const getPermissions = () => {
+    if (Platform.OS == 'android') {
+      PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+        PermissionsAndroid.PERMISSIONS.CAMERA,
+        PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+      ]);
+    }
+  };
+
+  useEffect(() => {
+    getPermissions();
+    let uikitSettings: UIKitSettings = {
+      appId: '264748bd2d2a1401',
+      authKey: 'ad3c0f58769c6d855ef0be84dcbac7e96d39625e',
+      region: 'in',
+      subscriptionType: CometChat.AppSettings.SUBSCRIPTION_TYPE_ALL_USERS,
+    };
+
+    CometChatUIKit.init(uikitSettings)
+      .then(() => {
+        console.log('CometChatUiKit successfully initialized');
+      })
+      .catch(error => {
+        console.log('Initialization failed with exception:', error);
+      });
+
+    // cometchat call app settings
+    const callAppSettings = new CometChatCalls.CallAppSettingsBuilder()
+      .setAppId('264748bd2d2a1401')
+      .setRegion('in')
+      .build();
+
+    CometChatCalls.init(callAppSettings).then(
+      () => {
+        console.log('CometChatCalls initialization completed successfully');
+      },
+      error => {
+        console.log('CometChatCalls initialization failed with error:', error);
+      },
+    );
+  }, []);
+
+  var listnerID = 'UNIQUE_LISTENER_ID_2';
+  CometChat.addCallListener(
+    listnerID,
+    new CometChat.CallListener({
+      onIncomingCallReceived: (call: any) => {
+        console.log('Incoming call:', call.sessionId);
+        incomingCall.current = call;
+        setCallReceived(true);
+      },
+      onOutgoingCallAccepted: (call: any) => {
+        console.log('Outgoing call accepted:', call.sessionId);
+        setCallAccepted(true);
+        setSessionId(call.sessionId);
+        setActiveCall(call);
+      },
+      onOutgoingCallRejected: (call: any) => {
+        console.log('Outgoing call rejected:', call.sessionId);
+        setCallRejected(true);
+        setCallReceived(false);
+        setCallAccepted(false);
+        setSessionId(null);
+        setActiveCall(call);
+        // dispatch({
+        //   type: CALL_UPDATED,
+        //   payload: {
+        //     ...callDetails,
+        //     callReceived: false,
+        //     callRejected: true,
+        //     call: call,
+        //   },
+        // });
+        // Outgoing Call Rejected
+      },
+      onIncomingCallCancelled: (call: any) => {
+        console.log('Incoming call calcelled:', call.sessionId);
+        setCallReceived(false);
+        setCallAccepted(false);
+        setCallRejected(false);
+        setActiveCall(call);
+        setSessionId(null);
+        // dispatch({
+        //   type: CALL_UPDATED,
+        //   payload: {
+        //     ...callDetails,
+        //     callReceived: false,
+        //     call: call,
+        //   },
+        // });
+      },
+      onCallEndedMessageReceived: (call: any) => {
+        console.log('CallEnded Message:', call.sessionId);
+      },
+    }),
+  );
+
+  const cancelCall = (c: any) => {
+    CometChat.rejectCall(c.sessionId, status).then(
+      call => {
+        console.log('Call rejected successfully', call);
+        setCallReceived(false);
+        setCallAccepted(false);
+        setCallRejected(false);
+        setActiveCall(null);
+        setSessionId(null);
+        // dispatch({
+        //   type: CALL_UPDATED,
+        //   payload: DEFAULT_CALL_DETAILS,
+        // });
+      },
+      error => {
+        console.log('Call rejection failed with error:', error);
+        setCallReceived(false);
+        setCallAccepted(false);
+        setCallRejected(false);
+        setActiveCall(null);
+        // dispatch({
+        //   type: CALL_UPDATED,
+        //   payload: {
+        //     ...callDetails,
+        //     callReceived: false,
+        //     call: null,
+        //   },
+        // });
+      },
+    );
+  };
+
+  const onAcceptHandler = (message: any) => {
+    console.log('Accepting Incoming call:', message);
+    generateCometChatCallToken(message.sessionId)
+      .then(callToken => {
+        CometChat.acceptCall(message.sessionId, callToken).then(
+          call => {
+            console.log('Call accepted successfully:', call);
+            setCallAccepted(true);
+            setSessionId(call.sessionId);
+            setActiveCall(call);
+            setCallReceived(false);
+            // dispatch({
+            //   type: CALL_UPDATED,
+            //   payload: {
+            //     ...callDetails,
+            //     callReceived: false,
+            //     callAccepted: true,
+            //     callToken: callToken,
+            //     call: call,
+            //   },
+            // });
+          },
+          error => {
+            console.log('Call acceptance failed with error:', error);
+            setCallReceived(false);
+            setCallAccepted(false);
+            setCallRejected(false);
+            setActiveCall(null);
+            setSessionId(null);
+            // dispatch({
+            //   type: CALL_UPDATED,
+            //   payload: DEFAULT_CALL_DETAILS,
+            // });
+            Alert.alert('Error occured on incoming call:', error);
+          },
+        );
+      })
+      .catch(error => {
+        setCallReceived(false);
+        setCallAccepted(false);
+        setCallRejected(false);
+        setActiveCall(null);
+        setSessionId(null);
+
+        // dispatch({
+        //   type: CALL_UPDATED,
+        //   payload: DEFAULT_CALL_DETAILS,
+        // });
+        console.log('Error occured on incoming call:', error);
+        Alert.alert('Error occured on incoming call:', error);
+      });
+  };
+
+  const onErrorHandler = (error: any) => {
+    console.log('Error occured on incoming call:', error);
+    setCallReceived(false);
+    setCallAccepted(false);
+    setCallRejected(false);
+    setActiveCall(null);
+    setSessionId(null);
+    // dispatch({
+    //   type: CALL_UPDATED,
+    //   payload: DEFAULT_CALL_DETAILS,
+    // });
+    //code
+  };
+
+  const audioOnly = false;
+  const deafaultLayout = true;
+
+  const callListener = new CometChatCalls.OngoingCallListener({
+    onUserJoined: user => {
+      console.log('user joined:', user);
+    },
+    onUserLeft: user => {
+      console.log('user left:', user);
+    },
+    onUserListUpdated: userList => {
+      console.log('user list:', userList);
+    },
+    onCallEnded: () => {
+      console.log('Call ended');
+    },
+    onCallEndButtonPressed: () => {
+      console.log('End Call button pressed');
+    },
+    onError: error => {
+      console.log('Call Error: ', error);
+    },
+    onAudioModesUpdated: audioModes => {
+      console.log('audio modes:', audioModes);
+    },
+    onCallSwitchedToVideo: event => {
+      console.log('call switched to video:', event);
+    },
+    onUserMuted: event => {
+      console.log('user muted:', event);
+    },
+  });
+
+  const callSettings = new CometChatCalls.CallSettingsBuilder()
+    .enableDefaultLayout(deafaultLayout)
+    .setIsAudioOnlyCall(audioOnly)
+    .setCallEventListener(callListener)
+    .setDefaultAudioMode(CometChatCalls.AUDIO_MODE.SPEAKER);
+
+  const generateCometChatCallToken = async (sessionId: string) => {
+    return new Promise(async (resolve, reject) => {
+      let loggedInUser: any = await CometChat.getLoggedinUser();
+      let authToken = loggedInUser.getAuthToken();
+      CometChatCalls.generateToken(sessionId, authToken)
+        .then(res => {
+          resolve(res.token);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  };
+
+  const handleJoinCall = () => {
+    // cometchat
+    const receiverId = 'aman_mehra_sebi_1727586077424';
+    const callType = CometChat.CALL_TYPE.VIDEO;
+    const receiverType = CometChat.RECEIVER_TYPE.USER;
+    const callObject = new CometChat.Call(receiverId, callType, receiverType);
+
+    CometChat.initiateCall(callObject)
+      .then((c: any) => {
+        console.log('Call initiated successfully:', c.sessionId);
+        setActiveCall(c);
+        setSessionId(c.sessionId);
+      })
+      .catch(console.log);
+  };
+
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
@@ -71,26 +364,70 @@ function App(): React.JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+        {/* add an input field and a button to login to cometchat */}
+        <View>
+          <Button
+            title="Login to CometChat as Aman"
+            onPress={() => {
+              CometChatUIKit.login({uid: 'aman_mehra_sebi_1727586077424'}).then(
+                (user: any) => {
+                  console.log('Login Successful:', {user});
+                  setCometchatUser(user);
+                },
+                error => {
+                  console.log('Login failed with exception:', {error});
+                },
+              );
+            }}
+          />
+          <Button
+            title="Login to CometChat as RK Mehra"
+            onPress={() => {
+              CometChatUIKit.login({uid: 'ramkalyan_mehra_1727587369044'}).then(
+                (user: any) => {
+                  console.log('Login Successful:', {user});
+                  setCometchatUser(user);
+                },
+                error => {
+                  console.log('Login failed with exception:', {error});
+                },
+              );
+            }}
+          />
         </View>
+
+        {/* comet chat logged in username */}
+        {cometchatUser && (
+          <View>
+            <Text>Logged in as: {cometchatUser?.name || ''}</Text>
+          </View>
+        )}
+
+        {callReceived && (
+          <CometChatIncomingCall
+            call={incomingCall.current}
+            onDecline={cancelCall}
+            onAccept={onAcceptHandler}
+            onError={onErrorHandler}
+          />
+        )}
+
+        {sessionId && callAccepted && (
+          <CometChatOngoingCall
+            sessionID={sessionId}
+            callSettingsBuilder={callSettings}
+          />
+        )}
+
+        {activeCall && (
+          <CometChatOutgoingCall
+            call={activeCall}
+            onDeclineButtonPressed={cancelCall}
+          />
+        )}
+
+        {/* create a button to start the call with a user of user id: aman_mehra_sebi_1727586077424  */}
+        <Button title="Start Call" onPress={handleJoinCall} />
       </ScrollView>
     </SafeAreaView>
   );
